5d5f61cbb8b99e306c1d3cd523495c1872f5a06c
pokuahnyarko-pixel <pokuahnyarko@gmail.com>
2025-09-17 01:21:43 +0000

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,8 +1,8 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~ZqkQVJrQ#asVS9fMcF_by2paIcgfluKo685btW4d6Cy856rJVEEg",  // add your Session Id here
   
-  OWNER_NUMBER: "263776388689", // put your phone number here
+  OWNER_NUMBER: "+23355 780 0775", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
