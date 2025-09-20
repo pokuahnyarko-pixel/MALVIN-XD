481342f069bf36a329c3a94787c334f9be13b773
pokuahnyarko-pixel <pokuahnyarko@gmail.com>
2025-09-20 12:32:20 +0000

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-  SESSION_ID: "malvin~ZqkQVJrQ#asVS9fMcF_by2paIcgfluKo685btW4d6Cy856rJVEEg",  // add your Session Id here
+  SESSION_ID: "malvin~Q7FgVYbJ#HISztdnjP7X2INRTkzqOXsPjBXeS5P624lb8PRgjLOM",  // add your Session Id here
   
   OWNER_NUMBER: "+23355 780 0775", // put your phone number here
   
